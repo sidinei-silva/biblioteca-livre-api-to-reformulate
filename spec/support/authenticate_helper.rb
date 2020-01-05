08e9060cfcd9d8acafca012a4f2fed57bd2e728e
Sidinei Silva <sidinei.dev@icloud.com>
2020-01-05 11:56:22 -0300

    Create Helper to Auth (Only Admin)

---
+++ spec/support/authenticate_helper.rb
@@ -0,0 +1,29 @@
+module Helpers
+  module Authenticate
+    def auth_as(user)
+      case user
+      when :admin
+        # Make Login
+        current_user = create(:admin)
+        headers = { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json'}
+        body = { email: current_user.email, password: current_user.password }.to_json
+        post api_v1_admin_session_path, headers: headers, params: body
+
+        # Get token
+        client = response.headers['client']
+        token = response.headers['access-token']
+        expiry = response.headers['expiry']
+        token_type = response.headers['token-type']
+        uid = response.headers['uid']
+
+        auth_params = {
+            'access-token' => token,
+            'client' => client,
+            'uid' => uid,
+            'expiry' => expiry,
+            'token_type' => token_type
+        }
+      end
+    end
+  end
+end
