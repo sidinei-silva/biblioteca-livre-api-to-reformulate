0bfdb00c09e79df1257bd3fc3129b0572abc99a7
Sidinei Silva <sidinei.dev@icloud.com>
2020-01-05 15:07:00 -0300

    Include login with manager in helper

--- spec/support/authenticate_helper.rb
+++ spec/support/authenticate_helper.rb
@@ -23,6 +23,27 @@ module Helpers
             'expiry' => expiry,
             'token_type' => token_type
         }
+      when :manager
+        # Make Login
+        current_user = create(:manager)
+        headers = {'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json'}
+        body = {email: current_user.email, password: current_user.password}.to_json
+        post '/api/v1/manager_auth/sign_in', headers: headers, params: body
+
+        # Get token
+        client = response.headers['client']
+        token = response.headers['access-token']
+        expiry = response.headers['expiry']
+        token_type = response.headers['token-type']
+        uid = response.headers['uid']
+
+        auth_params = {
+            'access-token' => token,
+            'client' => client,
+            'uid' => uid,
+            'expiry' => expiry,
+            'token_type' => token_type
+        }
       end
     end
   end
