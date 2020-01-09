5c6bde73e12a83226f3d13d5cf763db108790ae7
Sidinei Silva <sidinei.dev@icloud.com>
2020-01-09 13:36:56 -0300

    Include value true in default to avaliable book

--- db/migrate/20200109163447_create_books.rb
+++ db/migrate/20200109163447_create_books.rb
@@ -3,7 +3,7 @@ class CreateBooks < ActiveRecord::Migration[6.0]
     create_table :books do |t|
       t.string :name
       t.string :year
-      t.boolean :avaliable
+      t.boolean :avaliable, default: true
       t.references :author, null: false, foreign_key: true
 
       t.timestamps
