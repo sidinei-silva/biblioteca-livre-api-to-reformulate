4022da928b3785cea9a3fcb5950deb0c6065bf3e
Sidinei Silva <sidinei.dev@icloud.com>
2020-01-10 10:32:16 -0300

    Include url files to cover and file in book serializer

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -1,4 +1,17 @@
 class BookSerializer < ActiveModel::Serializer
-  attributes :id, :name, :year, :avaliable
+  #Helpers for urls in blob active storage
+  include Rails.application.routes.url_helpers
+
+
+  attributes :id, :name, :year, :avaliable, :cover, :file
   belongs_to :author
+
+  def cover
+    rails_blob_url(object.cover, only_path: true) if object.cover.attached?
+  end
+
+  def file
+    rails_blob_url(object.file, only_path: true) if object.file.attached?
+  end
+
 end
